a56d064eaa5978c2b246090b20235d131013b9b0
cwlucas41 <cwlucas41@gmail.com>
2016-04-05 18:04:32 -0500

    changed LOCass to use Strings and not Enums
    LO won't know what the enums are, so there is a string to enum conversion in the class

--- WSD/src/cass/LO/LibreOfficeCass.java
+++ WSD/src/cass/LO/LibreOfficeCass.java
@@ -10,12 +10,33 @@ public class LibreOfficeCass {
 	
 	private WSD wsd;
 	
-	public LibreOfficeCass(String leftContext, String target, String rightContext, Language language) {
-		wsd = new WSD(leftContext, target, rightContext, language);
+	public LibreOfficeCass(String leftContext, String target, String rightContext, String language) {
+		Language enumLang = null;
+		
+		switch (language) {
+		case "English":
+			enumLang = Language.EN;
+			break;
+
+		default:
+			break;
+		}
+		
+		wsd = new WSD(leftContext, target, rightContext, enumLang);
 	}
 	
-	public String[][] getSynonyms(Algorithm algorithm) {
-		return convertToArrayOfArrays(wsd.rankSynsetsUsing(algorithm));
+	public String[][] getSynonyms(String algorithm) {
+		Algorithm enumAlg = null;
+		
+		switch (algorithm) {
+		case "LeskWithWordNet":
+			enumAlg = Algorithm.LESK_WITH_WORDNET;
+			break;
+
+		default:
+			break;
+		}
+		return convertToArrayOfArrays(wsd.rankSynsetsUsing(enumAlg));
 	}
 	
 	private String[][] convertToArrayOfArrays(List<Set<String>> sortedSynonymSets) {
