908adf26a95dc0c2568827b3c5f753782fd6447b
cwlucas41 <cwlucas41@gmail.com>
2016-04-20 01:58:26 -0500

    added temporary JUnit test practicing using JSemcor
    the test open a Semcor resource (the br-a01 context in the brown1 concord)
    for each sentence in the context, it finds the first sense-tagged word, its sense, and surrounding context. These are all printed to the console
    
    in future commits, this functionality should be expanded to not find the first sense-tagged word as the target. Also, this should be packaged as a class instead of a JUnit test. The class will be a resource in testing to get an example test set for the algorithms and the correct sense for the word.

--- WSD/test/cass/wsd/TestLesk.java
+++ WSD/test/cass/wsd/TestLesk.java
@@ -2,14 +2,26 @@ package cass.wsd;
 
 import static org.junit.Assert.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 
 import cass.languageTool.Language;
+import edu.mit.jsemcor.detokenize.DefaultDetokenizer;
+import edu.mit.jsemcor.element.IContext;
+import edu.mit.jsemcor.element.ISentence;
+import edu.mit.jsemcor.element.IWordform;
+import edu.mit.jsemcor.main.IConcordance;
+import edu.mit.jsemcor.main.IConcordanceSet;
+import edu.mit.jsemcor.main.Semcor;
 
 public class TestLesk {
+	
+	DefaultDetokenizer detokenizer =  new DefaultDetokenizer();
 
 	@Test
 	public void test() {
@@ -24,6 +36,82 @@ public class TestLesk {
 			assertEquals(properScore.get(i), (Integer) ranked.get(i).getScore());
 			
 		}
+	}
+	
+	@Test
+	public void test2() {
+
+	    // construct the URL to the Semcor directory
+		String path = "semcor3.0";
+		URL url = null;
+		try {
+			url = new URL("file", null, path);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// construct the semcor object and open it
+		IConcordanceSet semcor = new Semcor(url);
+		semcor.open();
+		
+		IConcordance concord = semcor.get("brown1");
+		IContext context = concord.getContext("br-a01");
+		
+		for (ISentence sentence : context.getSentences()) {
+	
+			List<String> leftTokens = new ArrayList<String>();
+			String target = null, pos = null;
+			List<String> rightTokens = new ArrayList<String>();
+			
+			boolean targetIsFound = false;
+			for (IWordform wf : sentence.getWordList()) {
+				
+				if (!targetIsFound) {
+					if (wf.getSemanticTag() != null) {
+						target = concatenateTokens(wf.getConstituentTokens());
+						pos = wf.getPOSTag().getValue();
+						targetIsFound = true;
+					} else {
+						leftTokens.addAll(wf.getConstituentTokens());
+					}
+				} else {
+					rightTokens.addAll(wf.getConstituentTokens());
+				}
+			}
+			
+			String leftContext = concatenateTokens(leftTokens);
+			String rightContext = concatenateTokens(rightTokens);
+			
+			System.out.println(leftContext);
+			System.out.println(pos + "\t" + target);
+			System.out.println(rightContext);
+			System.out.println();
+			
+		}
+	}
+	
+	private String concatenateTokens(List<String> tokens) {
+		int numTokens = tokens.size();
+		String concatenated = "";
+		if (numTokens != 0) {
+		
+			concatenated += tokens.get(0);
+		
+			if (numTokens > 1) {
+				String sep = null;
+				for (int j = 1; j < tokens.size(); j++) {
+					String token = tokens.get(j);
+					
+					sep = detokenizer.getSeparatorString(concatenated, token);
+					if (sep != null) {
+						concatenated += sep;
+					}
+					concatenated += token;
+				}
+			}
+		}
+		return concatenated;
 	}
 
 }
