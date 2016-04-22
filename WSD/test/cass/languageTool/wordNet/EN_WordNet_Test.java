943f33aaf45201e79d9b4b4c38b99437afef3a55
TEWatson <taylorwatson6895@yahoo.com>
2016-04-22 14:40:33 -0500

    Began debugging EN_WordNet with EN_WordNet_Test

--- WSD/test/cass/languageTool/wordNet/EN_WordNet_Test.java
+++ WSD/test/cass/languageTool/wordNet/EN_WordNet_Test.java
@@ -12,11 +12,22 @@ public class EN_WordNet_Test {
 	
 	@Test
 	public void getSensesTest() {
-		Set<CASSWordSense> senses = wn.getSenses("java");
+		Set<CASSWordSense> senses = wn.getSenses("basketball");
 		for (CASSWordSense sense : senses) {
 			assertNotNull(sense);
 			System.out.println(sense.getId());
+			System.out.println(sense.getPOS());
 		}
 	}
 
+	@Test
+	public void getDefinitionTest() {
+		Set<CASSWordSense> senses = wn.getSenses("basketball");
+		for (CASSWordSense sense : senses) {
+			String gloss = wn.getDefinition(sense);
+			assertNotNull(gloss);
+			System.out.println(gloss);
+		}
+	}
+	
 }
