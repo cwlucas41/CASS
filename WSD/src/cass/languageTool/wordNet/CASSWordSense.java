441bb3a08c9e186f9f6c490fede5145bbde68df1
cwlucas41 <cwlucas41@gmail.com>
2016-04-22 22:33:16 -0500

    added sense number field to CASSWordSense

--- WSD/src/cass/languageTool/wordNet/CASSWordSense.java
+++ WSD/src/cass/languageTool/wordNet/CASSWordSense.java
@@ -1,26 +1,47 @@
 package cass.languageTool.wordNet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CASSWordSense {
 	
-	private String full_id;
+	private List<String> synsetIDs;
 	private String target;
 	private String part_of_speech;
+	private int senseNumber;
 	
-	public CASSWordSense(String target, String id, String pos) {
+	public CASSWordSense(String target, String id, String pos, int senseNumber) {
 		this.target = target;
-		this.full_id = id;
+		this.synsetIDs = new ArrayList<String>();
+		synsetIDs.add(id);
 		this.part_of_speech = pos;
+		this.senseNumber = senseNumber;
+	}
+	
+	public CASSWordSense(String target, List<String> ids, String pos, int senseNumber) {
+		this.target = target;
+		this.synsetIDs = ids;
+		this.part_of_speech = pos;
+		this.senseNumber = senseNumber;
+	}
+	
+	public void addSynsetID(String id) {
+		synsetIDs.add(id);
 	}
 
 	public String getTarget() {
 		return target;
 	}
 	
-	public String getId() {
-		return full_id;
-	}
-	
 	public String getPOS() {
 		return part_of_speech;
+	}
+
+	public List<String> getSynsetIDs() {
+		return synsetIDs;
+	}
+
+	public int getSenseNumber() {
+		return senseNumber;
 	}
 }
