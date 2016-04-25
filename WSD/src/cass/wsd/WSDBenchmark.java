32ed8831fdc2702419d20b863e82873f267edb76
cwlucas41 <cwlucas41@gmail.com>
2016-04-25 15:46:06 -0500

    implemented binning

--- WSD/src/cass/wsd/WSDBenchmark.java
+++ WSD/src/cass/wsd/WSDBenchmark.java
@@ -14,7 +14,7 @@ public class WSDBenchmark {
 	public void benchmark(Algorithm algorithm) {
 		Iterator<TestData> tsg = new TestSentenceGenerator("semcor3.0");
 				
-		Map<Integer,HashMap<Integer,Integer>> bins = new HashMap<Integer, HashMap<Integer, Integer>>();
+		Map<Integer,Map<Integer,Integer>> bins = new HashMap<Integer, Map<Integer, Integer>>();
 		
 		int numCorrect = 0;
 		int numSentences = 0;
@@ -26,11 +26,22 @@ public class WSDBenchmark {
 			
 			int numberOfSenses = results.size();
 			
-			if (!results.isEmpty() && ts.getSenses().contains(results.get(0).getSense().getId())) {
-				numCorrect++;
+			// ensure bin exists
+			bins.putIfAbsent(numberOfSenses, new HashMap<Integer, Integer>());
+			
+			Map<Integer, Integer> bin = bins.get(numberOfSenses);
+			
+			for (ScoredSense result : results) {
+				if (ts.getSenses().contains(result.getSense().getId())) {
+					int indexOfCorrectSense = results.indexOf(result);
+					Integer currentCount = bin.get(indexOfCorrectSense);
+					if (currentCount == null) {
+						bin.put(indexOfCorrectSense, 1);
+					} else {
+						bin.put(indexOfCorrectSense, currentCount++);
+					}
+				}
 			}
-
-			numSentences++;
 			
 			if (numSentences % 10 == 0) {
 				System.out.println(numCorrect + " / " + numSentences);
