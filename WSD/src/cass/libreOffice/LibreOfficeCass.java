9ba1730c74be8ca9c36d613694cfa5d3bb24f80a
cwlucas41 <cwlucas41@gmail.com>
2016-04-28 15:06:11 -0500

    JavaDoced LibreOfficeCass

--- WSD/src/cass/libreOffice/LibreOfficeCass.java
+++ WSD/src/cass/libreOffice/LibreOfficeCass.java
@@ -6,10 +6,22 @@ import cass.languageTool.Language;
 import cass.languageTool.wordNet.CASSWordSense;
 import cass.wsd.*;
 
+/**
+ * Wrapper class for LibreOffice Basic to interact with. Translates calls into WSD calls and translates returns from List to array types.
+ * @author cwlucas41
+ *
+ */
 public class LibreOfficeCass {
 	
 	private WSD wsd;
 	
+	/**
+	 * Constructor for LibreOfficeCass. Converts language String into Language Enumeration type.
+	 * @param leftContext - String of words left of the target word in corpus
+	 * @param target - target word String
+	 * @param rightContext - String of words right of the target word in corpus
+	 * @param language - String representing language of test
+	 */
 	public LibreOfficeCass(String leftContext, String target, String rightContext, String language) {
 		Language enumLang = null;
 		
@@ -25,6 +37,12 @@ public class LibreOfficeCass {
 		wsd = new WSD(leftContext, target, rightContext, enumLang);
 	}
 	
+	/**
+	 * Calls WSD algorithm using a string for the algorithm field (translates into Algorithm Enum). 
+	 * Then gets synonyms for each sense and translates result from List type to array type
+	 * @param algorithm String
+	 * @return String array of senses containing synonyms
+	 */
 	public String[][] getSynonyms(String algorithm) {
 		List<CASSWordSense> rankedSenses = null;
 		
@@ -40,6 +58,11 @@ public class LibreOfficeCass {
 		return convert(rankedSenses);
 	}
 
+	/**
+	 * Converts list of senses to nested array of synonyms
+	 * @param senses
+	 * @return String array for senses containg array for synonyms
+	 */
 	private String[][] convert(List<CASSWordSense> senses) {
 		return null;
 	}
